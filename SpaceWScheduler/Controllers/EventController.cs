eef8fe912b1bdb5f50e46532f5780905b6e62bb1
wymtowne <wymtowne@gmail.com>
2023-03-24 22:33:19 -0700

    Added endpoints to event controller

--- SpaceWScheduler/Controllers/EventController.cs
+++ SpaceWScheduler/Controllers/EventController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SpaceWScheduler.Models.Interfaces;
+using SpaceWScheduler.Models.Models;
 
 namespace SpaceWScheduler.Controllers
 {
@@ -7,6 +9,32 @@ namespace SpaceWScheduler.Controllers
     [ApiController]
     public class EventController : ControllerBase
     {
+
+        private readonly ILogger<EventController> _logger;
+        private readonly IEventGetter _eventGetter;
+        private readonly IEventUpdater _eventUpdater;
+
+        public EventController(
+            ILogger <EventController> logger,
+            IEventGetter eventGetter,
+            IEventUpdater eventUpdater
+        ) 
+        {
+            _logger = logger;
+            _eventGetter = eventGetter;
+            _eventUpdater = eventUpdater;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Event>> GetAllEvents() =>
+            _eventGetter.GetEvents().ToList();
+        
+
+        [HttpGet]
+        [Route("{eventId}")]
+        public ActionResult<Event?> GetEvent([FromRoute] int eventId) =>
+            _eventGetter.GetEvent(eventId);
+
 
     }
 }
