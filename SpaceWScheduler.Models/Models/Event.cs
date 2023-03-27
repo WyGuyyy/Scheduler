a6b787fce2636cae3198f35338f9a982c15f67af
wymtowne <wymtowne@gmail.com>
2023-03-26 18:16:10 -0700

    Added Schedule navigation property to Event

--- SpaceWScheduler.Models/Models/Event.cs
+++ SpaceWScheduler.Models/Models/Event.cs
@@ -14,6 +14,7 @@ namespace SpaceWScheduler.Models.Models
         public DateTime? EndTime { get; set; }
         public string? Name { get; set; }
         public int ScheduleId { get; set; }
+        public Schedule Schedule { get; set; }
 
         [JsonConstructor]
         public Event() { }
