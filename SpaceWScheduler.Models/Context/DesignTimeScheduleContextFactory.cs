88337941e6a7c92677ca56c616632e82d916a165
wymtowne <wymtowne@gmail.com>
2023-03-27 22:44:34 -0700

    Created design time factory to allow ef core tools to perform migrations on the database

---
+++ SpaceWScheduler.Models/Context/DesignTimeScheduleContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace SpaceWScheduler.Models.Context
+{
+    public class DesignTimeScheduleContextFactory : IDesignTimeDbContextFactory<SchedulerContext>
+    {
+        public SchedulerContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<SchedulerContext>();
+            optionsBuilder.UseSqlite("Data Source=Scheduler.db");
+
+            return new SchedulerContext(optionsBuilder.Options);
+        }
+    }
+}
